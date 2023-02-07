cad1a03dee5849872708643dd637ba2976358153
Pablo Timoner <Pablo.Timoner@unige.ch>
2023-02-07 21:27:09 +0100

    -W

--- main_hpc.sh
+++ main_hpc.sh
@@ -2,10 +2,10 @@
 # $2 parameter correspond to the path to the singularity AccessMod image
 # to download the image e.g. singularity pull accessmod.sif docker://fredmoser/accessmod:5.8.0
 echo 'Getting regions...'
-sbatch -o ./slurm_reports/regions.out --wait script_hpc_get_regions.sh $1 $2 $3
+sbatch -W -o ./slurm_reports/regions.out script_hpc_get_regions.sh $1 $2 $3
 REGIONS=$(cat $1/inputs.json | jq -r '.index | join(",")')
 echo $REGIONS
 REGIONS=$(echo 2)
 mkdir -p slurm_reports
 echo 'Submitting main analyses (job array)...'
-sbatch --dependency=singleton -a $REGIONS -o ./slurm_reports/%a_%A.out script_hpc.sh $1 $2
+sbatch -a $REGIONS -o ./slurm_reports/%a_%A.out script_hpc.sh $1 $2

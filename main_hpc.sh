2eefa274bd61df16c04b6ac3b96d89f6c695de27
Pablo Timoner <Pablo.Timoner@unige.ch>
2023-02-07 23:14:11 +0100

    mkdir

--- main_hpc.sh
+++ main_hpc.sh
@@ -2,10 +2,11 @@
 # $2 parameter correspond to the path to the singularity AccessMod image
 # to download the image e.g. singularity pull accessmod.sif docker://fredmoser/accessmod:5.8.0
 echo 'Getting regions: please wait...'
+mkdir -p slurm_reports
 sbatch -W -o ./slurm_reports/regions.out script_hpc_get_regions.sh $1 $2 $3
 REGIONS=$(cat $1/inputs.json | jq -r '.index | join(",")')
 echo $REGIONS
 # REGIONS=$(echo 2)
-mkdir -p slurm_reports
+
 # echo 'Submitting main analyses (job array)...'
 sbatch -a $REGIONS -o ./slurm_reports/%a_%A.out script_hpc.sh $1 $2

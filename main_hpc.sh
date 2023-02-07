b42c480f853bef3bcf6e7d630408d370eb168408
Pablo Timoner <Pablo.Timoner@unige.ch>
2023-02-07 16:26:37 +0100

    index

--- main_hpc.sh
+++ main_hpc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-SERVICES=$(cat $1/inputs.json | jq -r '.region | join(",")')
-echo $SERVICES
+REGIONS=$(cat $1/inputs.json | jq -r '.index | join(",")')
+echo $REGIONS
 # $1 parameter correspond to the path to the singularity AccessMod image
 # to download the image e.g. singularity pull accessmod.sif docker://fredmoser/accessmod:5.8.0
 # mkdir -p slurm_reports
-# sbatch -a $SERVICES -o ./slurm_reports/%a_%A.out script_hpc.sh $1 $2
+# sbatch -a $REGIONS -o ./slurm_reports/%a_%A.out script_hpc.sh $1 $2

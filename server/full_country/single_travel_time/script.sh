3ea61393451d0d62d773d1c55c9ae050dbdd7809
Pablo Timoner <Pablo.Timoner@unige.ch>
2023-03-24 09:57:25 +0100

    run container as non-root user bind /data folder

--- server/full_country/single_travel_time/script.sh
+++ server/full_country/single_travel_time/script.sh
@@ -4,10 +4,16 @@
 IMAGE=$2
 
 # Output folder
+# As the container will be run as a non-root user, we need to bind the /data folder
+# If not, we don't have the right to access it (by default volume mounted to the root)
 mkdir -p $1/out
+mkdir -p $1/AMdata/logs
+mkdir -p $1/AMdata/cache
+mkdir -p $1/AMdata/dbgrass
 
 # Inputs
 OUTPUT_DIR=$1/out
+DATA_DIR=$1/AMdata
 PROJECT_FILE=$1/project.am5p
 R_SCRIPT_FILE='./script.R'
 CONFIG_FILE=$1/config.json
@@ -29,8 +35,12 @@ check_file "$R_SCRIPT_FILE"
 check_file "$CONFIG_FILE"
 
 # Run docker with mounted inputs and launch the R script
+# --rm clean up the container
+# --user so the docker container is run as a non-root user (to keep the rights on the outputs)
 docker run \
   --rm \
+  --user $(id -u):$(id -g) \
+  -v $DATA_DIR:/data \
   -v $OUTPUT_DIR:/batch/out \
   -v $PROJECT_FILE:/batch/project.am5p \
   -v $CONFIG_FILE:/batch/config.json \

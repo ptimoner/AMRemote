99c5b502b9587ed5b8b719d56f1046a1024e8737
Pablo Timoner <Pablo.Timoner@unige.ch>
2023-03-29 18:44:40 +0200

    --array

--- sh/array.sh
+++ sh/array.sh
@@ -96,5 +96,5 @@ then
   sbatch --dependency=afterok:${JOB_ARRAY_ID} --output "$OUTPUT_DIR/slum_reports/replay.out" --job-name="$JOB_NAME" "$RUN_DIR/sh/replaySingularity.sh" "${PARAM[@]}"
 else
   # Job array
-  sbatch --dependency=afterok:${JOB_ARRAY_ID} --array=$JOB_ARRAY_ID --output --job-name="$JOB_NAME" "$OUTPUT_DIR/slum_reports/replay_%a_%A.out" "$RUN_DIR/sh/replaySingularity.sh" "${PARAM[@]}"
+  sbatch --dependency=afterok:${JOB_ARRAY_ID} --array=$JOB_ARRAY_ID --output "$OUTPUT_DIR/slum_reports/replay_%a_%A.out" --job-name="$JOB_NAME" "$RUN_DIR/sh/replaySingularity.sh" "${PARAM[@]}"
 fi
